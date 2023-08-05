4438ada6d8caa969bb7641dfea098a287dab3ac8
Mikhail Baranov <problemaprod@gmail.com>
2023-08-05 16:36:54 +0100

    Выполнил домашнее задание  22.1

---
+++ main/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from main.models import Product, Version
+
+banned_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар', ]
+
+
+class FormStileMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(FormStileMixin,forms.ModelForm):
+    class Meta:
+        model = Product
+        exclude = ('make_date', 'change_date',)
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+
+        for banned_word in banned_list:
+            if banned_word in cleaned_data.lower():
+                raise forms.ValidationError('Нельзя добавть этот товар')
+                break
+            else:
+                continue
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+
+        for banned_word in banned_list:
+            if banned_word in cleaned_data.lower():
+                raise forms.ValidationError('Нельзя добавть этот товар')
+                break
+            else:
+                continue
+        return cleaned_data
+
+
+class VersionForm(FormStileMixin,forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
--- main/models.py
+++ main/models.py
@@ -24,8 +24,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='product/', verbose_name='Превью', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
     price = models.IntegerField(verbose_name='Цена')
-    make_date = models.DateField(verbose_name='Дата создания', **NULLABLE)
-    change_date = models.DateField(verbose_name='Дата последнего изменения', **NULLABLE)
+    make_date = models.DateField(verbose_name='Дата создания', auto_now_add=True, **NULLABLE)
+    change_date = models.DateField(verbose_name='Дата последнего изменения', auto_now_add=True, **NULLABLE)
 
     def __str__(self):
         return f'{self.name}'
@@ -34,3 +34,16 @@ class Product(models.Model):
         verbose_name = 'Продукт'
         verbose_name_plural = 'продукты'
         ordering = ('name', 'category', 'description',)
+
+
+class Version(models.Model):
+    product_name = models.ForeignKey(Product, verbose_name='название товара', on_delete=models.CASCADE)
+    version_number = models.IntegerField(verbose_name='номер версии')
+    version_name = models.CharField(max_length=150, verbose_name='название версии')
+    is_active = models.BooleanField(default=True, verbose_name='активная версяи')
+
+    def __str__(self):
+        return self.version_name
+    class Meta:
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
